ec8a2c3bc55284e9a0e6a13f821449f222415f00
Tmanyo <trent@lasich.com>
2016-11-30 19:47:23 -0500

    Fixed code and added a manipulation feature.

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
---Shows a message in chat when a player kills another player.
+-- Shows a message in chat when a player kills another player.
 minetest.register_on_punchplayer(function(player, hitter, tool_capabilities, damage)
      local player_hit = player:get_player_name()
      local criminal = hitter:get_player_name()
@@ -11,33 +11,51 @@ minetest.register_on_punchplayer(function(player, hitter, tool_capabilities, dam
      end)
 end)
 
---Shows a connecting message when someone is connecting.
+-- Shows a connecting message when someone is connecting.
 minetest.register_on_prejoinplayer(function(name, ip)
      minetest.chat_send_all(name .." is connecting!")
 end)
 
 -- Server Reboot.
 minetest.register_chatcommand("reboot", {
-      description = "Warn players and reboot server",
-      func = function(name, param)
-			if minetest.check_player_privs(name, {server=true}) == true then
-					minetest.chat_send_all("[Server] Rebooting in 1 minute!")
-          minetest.after(10, function()
-              minetest.chat_send_all("[Server] Rebooting in 50 seconds!")
-          minetest.after(10, function()
-              minetest.chat_send_all("[Server] Rebooting in 40 seconds!")
-          minetest.after(10, function()
-              minetest.chat_send_all("[Server] Rebooting in 30 seconds!")
-          minetest.after(10, function()
-              minetest.chat_send_all("[Server] Rebooting in 20 seconds!")
-          minetest.after(10, function()
-              minetest.chat_send_all("[Server] Rebooting in 10 seconds!")
-					minetest.request_shutdown()
-                    end)
-                  end)
-                end)
-              end)
-            end)
+     description = "Warn players and reboot server",
+     func = function(name, param)
+          if minetest.check_player_privs(name, {server=true}) == true then
+               minetest.chat_send_all("[Server] Rebooting in 1 minute!")
+               minetest.after(60, function()
+                    minetest.request_shutdown()
+               end)
+               minetest.after(10, function()
+                    minetest.chat_send_all("[Server] Rebooting in 50 seconds!")
+               end)
+               minetest.after(20, function()
+                    minetest.chat_send_all("[Server] Rebooting in 40 seconds!")
+               end)
+               minetest.after(30, function()
+                    minetest.chat_send_all("[Server] Rebooting in 30 seconds!")
+               end)
+               minetest.after(40, function()
+                    minetest.chat_send_all("[Server] Rebooting in 20 seconds!")
+               end)
+               minetest.after(50, function()
+                    minetest.chat_send_all("[Server] Rebooting in 10 seconds!")
+               end)
           end
-				end
+     end
+})
+
+-- Manipulate other players.
+minetest.register_chatcommand("manipulate", {
+     params = "<name> <text>",
+     description = "Say something as someone else if you have the server priv.",
+     func = function(name, params)
+          if minetest.check_player_privs(name, {server=true}) == true then
+               local s = params
+               local manipulated_name = s:match("%w+")
+               local manipulated_text = s:match(" %w+")
+               if minetest.check_player_privs(name, {server=true}) == true then
+                    minetest.chat_send_all("<" .. manipulated_name .. "> " .. manipulated_text)
+               end
+          end
+     end
 })
