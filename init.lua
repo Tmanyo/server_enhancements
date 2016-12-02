cdecfde24b555687f908f802a2fd037ffa74a212
Tmanyo <trent@lasich.com>
2016-12-01 19:50:31 -0500

    making changes

--- init.lua
+++ init.lua
@@ -114,6 +114,8 @@ minetest.register_on_joinplayer(function(player)
      staff = read_table_data()
      local playername = player:get_player_name()
      if staff.host == playername then
-          player:set_nametag_attributes("color=red")
+          player:set_nametag_attributes({
+               color = {r = 255, g = 0, b = 0}
+          })
      end
 end)
